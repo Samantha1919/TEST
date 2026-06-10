0575fedbef5aac94f898ed97ba90346d15b2f980
Samantha1919 <samantha.schif@gmail.com>
2026-06-10 09:51:27 +0200

    feat:add style

--- ArtGallery/app/src/main/java/com/example/bjr/MainActivity.kt
+++ ArtGallery/app/src/main/java/com/example/bjr/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -21,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bjr.ui.theme.ArtGalleryTheme
 
@@ -43,14 +46,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Affichage(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(), // on lui met fillMaxSize pour quil prenne toute la place pr quil centre vrm tout
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val textes = listOf("Chat un", "Chat deux", "Chat trois")
+        val textes = listOf("Cookie", "Vera", "Rico")
         val images = listOf(R.drawable.cat_one, R.drawable.cat_two, R.drawable.cat_tree)
         var indexPrParcourirLeTableau by remember { mutableIntStateOf(0) }
-        val maxIndex = images.size - 1 // psq ducoup sa size est egale a 3 et vu que dans un array le premier index est egal a 0 bah ca fait 2 elements
+        val maxIndex =
+            images.size - 1 // psq ducoup sa size est egale a 3 et vu que dans un array le premier index est egal a 0 bah ca fait 2 elements
 
         Text(
             text = "Cats gallery",
@@ -61,34 +65,42 @@ fun Affichage(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(images[indexPrParcourirLeTableau]),
             contentDescription = null,
+            modifier = Modifier.padding(bottom = 20.dp)
         )
 
         Text(
             text = textes[indexPrParcourirLeTableau],
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 20.dp)
         )
 
-        Button(onClick = {
-            if (indexPrParcourirLeTableau == maxIndex) {
-                indexPrParcourirLeTableau = 0
-
-            } else {
-                indexPrParcourirLeTableau++
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth() // pour que la row prenne la largeur max et comme ca apres tu peux bouger les boutons car tu as la place
+        ) {
+            Button(onClick = {
+                if (indexPrParcourirLeTableau == 0) {
+                    indexPrParcourirLeTableau = maxIndex
+                } else {
+                    indexPrParcourirLeTableau--
+                }
+            }) {
+                Text(text = "Previous")
             }
-        }) {
-            Text(text = "Next")
-        }
 
-        Button(onClick = {
-            if (indexPrParcourirLeTableau == 0) {
-                indexPrParcourirLeTableau = maxIndex
-            } else {
-                indexPrParcourirLeTableau--
+            Button(onClick = {
+                if (indexPrParcourirLeTableau == maxIndex) {
+                    indexPrParcourirLeTableau = 0
+
+                } else {
+                    indexPrParcourirLeTableau++
+                }
+            }) {
+                Text(text = "Next")
             }
-        }) {
-            Text(text = "Previous")
-        }
 
+        }
     }
 }
 
