c09e3d00a72b8e5a199cf216ff932e74ab3b07ad
Samantha1919 <samantha.schif@gmail.com>
2026-06-23 16:14:12 +0200

    fear: ca marche pas style de tututututu

---
+++ Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.superheroes
+
+import SuperheroesTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Hero
+import com.example.superheroes.model.SuperHeroesDataSource.heroes
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperheroesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HeroApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroApp(modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(heroes) { hero ->
+            HeroItem(hero)
+        }
+    }
+}
+
+@Composable
+private fun HeroItem(
+    hero: Hero, // contient toutes les infos des heros
+
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) { // espace entre les cartes
+
+        Column(
+            modifier = Modifier.padding(8.dp),
+           // verticalArrangement = Arrangement.Center
+        ) { // espace/padding a linterieur de la carte
+
+            Text(
+                text = stringResource(hero.nameRes),
+                style = MaterialTheme.typography.displayMedium,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp, end = 8.dp, start = 8.dp),
+             //   textAlign = TextAlign.Center
+
+            )
+            Text(
+                text = stringResource(hero.descriptionRes),
+                modifier = Modifier.padding(bottom = 8.dp, end = 8.dp, start = 8.dp),
+                style = MaterialTheme.typography.labelSmall,
+              //  textAlign = TextAlign.Center
+            )
+
+
+            Box(
+             //   contentAlignment = Alignment.CenterEnd,
+                modifier = Modifier.fillMaxWidth()
+            ) { // les enfants de la box qui seront stylisées
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(8.dp))
+
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HeroPreview() {
+
+    HeroItem(
+        Hero(
+            nameRes = R.string.hero2,
+            descriptionRes = R.string.description2,
+            imageRes = R.drawable.android_superhero2
+        )
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Colonne() {
+    Column() {
+        Textes("bonjour", "salut")
+        Image(painter = painterResource(R.drawable.android_superhero2), contentDescription = null)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Rangee() {
+
+    Row() {
+
+        Textes("bonjour", "salut")
+
+        Image(painter = painterResource(R.drawable.android_superhero2), contentDescription = null)
+    }
+}
+
+@Composable
+private fun Textes(motUn: String, motDeux: String) {
+    Column() {
+    Text(text = motUn)
+    Text(text = motDeux)
+}
+}
+
