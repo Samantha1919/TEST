a4b5cf9cf4af3c4a88662d2cec33fe9df392a9c9
Samantha1919 <samantha.schif@gmail.com>
2026-06-29 11:41:02 +0200

    feat:add rights files and debut app

--- cupcake/app/build.gradle.kts
+++ cupcake/app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
 }
